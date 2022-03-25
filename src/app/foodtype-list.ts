21704264c8d064430061ffc0ab921e41526e4d11
KonlawatPach <pach.wongsrithong@Gmail.com>
2022-03-25 17:41:54 +0800

    add helloworld

---
+++ src/app/foodtype-list.ts
@@ -0,0 +1,14 @@
+export const foodtype = [
+    {
+        name: 'เมนูหลัก',
+        picture: ""
+    },
+    {
+        name: 'เครื่องดื่ม',
+        picture: ""
+    },
+    {
+        name: 'ขนมหวาน',
+        picture: ""
+    }
+]
