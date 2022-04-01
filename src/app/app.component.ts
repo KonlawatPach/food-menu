bb5fce0681d9073aeebbe556b36df384f7cbb928
KonlawatPach <pach.wongsrithong@Gmail.com>
2022-04-01 15:56:49 +0800

    bug fix sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,50 +8,8 @@ import { Component, Input } from '@angular/core';
 export class AppComponent {
   menu = [
             {
-                name: 'ข้าวผัดหมู',
-                picture: "https://www.trc-group.co.th/wp-content/uploads/2017/04/483.jpg",
-                normal: 30,
-                extra: 40
-            },
-            {
-                name: 'ข้าวผัดไก่',
-                picture: "https://img.wongnai.com/p/400x0/2020/03/24/9d4c062fdeaa40a6996fbefa553d52df.jpg",
-                normal: 30,
-                extra: 40
-            },
-            {
-                name: 'ข้าวผัดเนื้อ',
-                picture: "https://www.bloggang.com/data/narellan/picture/1426944823.jpg",
-                normal: 40,
-                extra: 50
-            },
-            {
-                name: 'ข้าวกระเพราหมู',
-                picture: "https://d12man5gwydfvl.cloudfront.net/wp-content/uploads/2019/03/%E0%B8%9C%E0%B8%B1%E0%B8%94%E0%B8%81%E0%B8%B0%E0%B9%80%E0%B8%9E%E0%B8%A3%E0%B8%B2%E0%B8%AB%E0%B8%A1%E0%B8%B9%E0%B8%AA%E0%B8%B1%E0%B8%9A-1.jpg",
-                normal: 35,
-                extra: 45
-            },
-            {
-                name: 'ข้าวกระเพราหมูกรอบ',
-                picture: "https://i.pinimg.com/originals/6f/9d/13/6f9d133ee92c028963ecac4edea4dd50.jpg",
-                normal: 40,
-                extra: 50
-            },
-            {
-                name: 'ข้าวกระเพราไก่',
-                picture: "https://i.pinimg.com/originals/0c/bc/66/0cbc66423688f2a914666989fcd9febd.jpg",
-                normal: 30,
-                extra: 40
-            },
-            {
-                name: 'ข้าวหมูกรอบ',
-                picture: "https://sls-prod.api-onscene.com/partner_files/trueidintrend/4567/111396344.jpg",
-                normal: 35,
-                extra: 45
-            },
-            {
-                name: 'ข้าวพริกแกงหมู',
-                picture: "https://live.staticflickr.com/5497/9713200629_eb0624fdd0_b.jpg",
+                name: 'ข้าวผัดแมว',
+                picture: "",
                 normal: 30,
                 extra: 40
             }
