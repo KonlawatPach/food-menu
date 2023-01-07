c0b04f4f1b97130e10aca4db69a4d8dfc710d7dd
KonlawatPach <pach.wongsrithong@Gmail.com>
2023-01-07 18:46:02 +0700

    Update foodlist.ts

--- src/app/foodlist.ts
+++ src/app/foodlist.ts
@@ -1,4 +1,4 @@
-export const foodlist = async () => await fetch("https://script.google.com/macros/s/AKfycbwBlF1WMuMpbGOuI8d5TawOq2flf_wkIGPuiQMUCb5HeZVlGjCyxY2YNjrdbU1_ppNC/exec")
+export const foodlist = async () => await fetch("https://script.google.com/macros/s/AKfycbylVa4kUOn9hxK4eariEg_7FtlLHtd8J3k93xNxq5PhhPH66Au8Ta8BOtqtuvZrSt6R-g/exec")
 .then(function(response) {
     return response.json();
 }).then( function(myJson:{name: string, picture: string, type: string, normal: number, extra: number, description: string}[]) {
